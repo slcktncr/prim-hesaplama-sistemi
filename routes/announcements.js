afc3232cf1ba2c768e503fb3c360f9c70200c86f
selcuk <selcuktcl@gmail.com>
2025-09-08 12:30:17 +0300

    comm38

--- routes/announcements.js
+++ routes/announcements.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const adminAuth = require('../middleware/adminAuth');
 const Announcement = require('../models/Announcement');
 const ActivityLog = require('../models/ActivityLog');
